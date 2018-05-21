8656a0355261405456cdbdb4606a14277d14c424
Valentin Baraise <valentin.baraise@nameshield.net>
2018-05-21 17:37:43 +0200

    First overview axios

--- wapt-webui/src/router/index.js
+++ wapt-webui/src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import AxiosTest from '@/components/AxiosTest'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'AxiosTest',
+      component: AxiosTest
+    },
+    {
+      path: '/test/Test',
+      name: 'AxiosTest',
+      component: AxiosTest
     }
   ]
 })
