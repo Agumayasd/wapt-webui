8e92bc227555d8a6bdf77360e37788ec18c26978
Tim <tim@clerc.im>
2018-05-22 22:12:30 +0200

    Delete default component

--- wapt-webui/src/router/index.js
+++ wapt-webui/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import AxiosTest from '@/components/AxiosTest'
 
 Vue.use(Router)
@@ -8,11 +7,6 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/test',
       name: 'AxiosTest',
