34df5a9bb7d495ac4e1bdb7f337ef588bc31e8ac
Timothée Clerc <tim@clerc.im>
2018-05-24 14:10:39 +0200

    Use router-link instead of href

--- wapt-webui/src/App.vue
+++ wapt-webui/src/App.vue
@@ -1,14 +1,14 @@
 <template>
 <div>
   <b-navbar toggleable type="light" variant="light">
-    <b-navbar-brand href="/Aguay">
+    <b-navbar-brand to="/Aguay">
         <img src="./assets/logo_wwui.png" class="d-inline-block align-top" alt="BV">
         Wapt Webui
     </b-navbar-brand>
 
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
-        <b-nav-item href="/Bootstrap">Hosts Status</b-nav-item>
+        <b-nav-item to="/Bootstrap">Hosts Status</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
