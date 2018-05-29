e5c7adbc8aa19417377d8bd6d1eff387cf0c7238
Aguay <baraise.valentin@gmail.com>
2018-05-29 08:13:41 +0200

    Add Dashboard and wapt store

---
+++ wapt-webui/src/store/modules/Wapt.js
@@ -0,0 +1,29 @@
+import { HTTP } from '@/utils/http'
+// initial state
+// shape: [{ id, quantity }]
+const state = {
+  waptState: []
+}
+
+const actions = {
+
+    LOAD_WAPT_JSON: function ({ commit }) {
+      HTTP.get('v1/hosts').then((response) => {
+        commit('STORE_WAPT', { list: response.data })
+      }, (err) => {
+        console.log(err)
+      })
+  }
+}
+
+const mutations = {
+  STORE_WAPT : (state, {list} ) => {
+    state.waptState = list
+  }
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
