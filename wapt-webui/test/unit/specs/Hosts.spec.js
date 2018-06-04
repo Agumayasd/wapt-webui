75a1282023f023a6a4875d0c19b2f7b62cdbb3dd
Tim <tim@clerc.im>
2018-06-04 22:36:35 +0200

    unit test

--- wapt-webui/test/unit/specs/Hosts.spec.js
+++ wapt-webui/test/unit/specs/Hosts.spec.js
@@ -1,22 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Hosts from '@/components/Hosts'
 
 describe('Hosts', () => {
   it('should render hosts table', () => {
-    const wrapper = shallowMount(Hosts, {
+    const wrapper = mount(Hosts, {
       data: {
-              items: [
-                      {
-                       computer_name: "computer55",
-                       connected_users: ["hkindellk6"],
-                       host_status: "ERROR",
-                       last_update_status: {date: "2018-05-18T13:32:42.138000"}
-                      }
-              ]
+        items: [
+          {
+            computer_name: "computer55",
+            connected_users: ["hkindellk6"],
+            host_status: "ERROR",
+            last_update_status: {date: "2018-05-18T13:32:42.138000"}
+          }
+        ]
       }
     })
-    console.log(wrapper)
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+    const vm = wrapper.vm
+    const table = wrapper.find('td')
+    const tdArray = wrapper.findAll('td')
+    const firstTd = tdArray.at(0)
+    const test = firstTd.vnode.children[0].children[0].text
+    console.log(test)
+    expect(test).toEqual('computer55')
   })
 })
