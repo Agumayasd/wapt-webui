f0e0e1e20b88a3589a5c7b34c40a0329be18f81a
Timothée Clerc <tim@clerc.im>
2018-06-05 14:00:54 +0200

    uit test

--- wapt-webui/test/unit/specs/Hosts.spec.js
+++ wapt-webui/test/unit/specs/Hosts.spec.js
@@ -1,26 +1,27 @@
 import { mount } from '@vue/test-utils'
 import Hosts from '@/components/Hosts'
 
-describe('Hosts', () => {
-  it('should render hosts table', () => {
-    const wrapper = mount(Hosts, {
-      data: {
+
+
+const data = {
         items: [
           {
             computer_name: "computer55",
-            connected_users: ["hkindellk6"],
+            connected_users: ['user01','user02'],
             host_status: "ERROR",
             last_update_status: {date: "2018-05-18T13:32:42.138000"}
           }
         ]
       }
-    })
-    const vm = wrapper.vm
-    const table = wrapper.find('td')
+
+describe('Hosts', () => {
+  it('should render table data with computer name', () => {
+    const wrapper = mount(Hosts, data)
+    console.log(data)
+
     const tdArray = wrapper.findAll('td')
-    const firstTd = tdArray.at(0)
-    const test = firstTd.vnode.children[0].children[0].text
-    console.log(test)
-    expect(test).toEqual('computer55')
+    const computerNameTd = tdArray.at(0)
+    const div = computerNameTd.find('div')
+    expect(div.text()).toEqual('computer55')
   })
 })
