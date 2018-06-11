45db16e9366e1080749301bad2ff914c4b8ca6ea
Aguay-val <val@aguay.eu>
2018-06-11 19:48:29 +0200

    WIP multiComponent Dahsboard

--- wapt-webui/src/components/Dashboard.vue
+++ wapt-webui/src/components/Dashboard.vue
@@ -1,63 +1,25 @@
 <template>
-<div>
-  <p>{{ count }}</p>
-  <p>{{ msg }}</p>
-  <p>
-    <button v-on:click="increment">+</button>
-    <button v-on:click="PrintThis">Show me</button>
-
-  </p>
-  <hr>
-  <tbody>
-    <!-- <tr v-for="host in hosts.result" :key="host.uuid">
-      <td>{{ host.uuid }}</td>
-      <td>{{ host.computer_fqdn }}</td>
-      <td>{{ host.os_name }}</td>
-    </tr> -->
-    <div> {{data}} </div>
-    <div> {{hostsOkStateCount}} </div>
-    <div> {{hostsErrorStateCount}} </div>
-    <div> {{hostsWarnStateCount}} </div>
-    <pie-chart :data="data" :colors="['#4e7300', '#ffa600', '#b51414']" width="800px" height="500px" :discrete="true"></pie-chart>
-
-  </tbody>
-</div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <pieHostsStatus></pieHostsStatus>
+      </b-col>
+      <b-col>
+        <serverStatus></serverStatus>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import pieHostsStatus from '@/components/dashboard/pieHostsStatus'
+import serverStatus from '@/components/dashboard/serverStatus'
 
 export default {
-  name: 'Dashobard',
-  mounted () {
-    this.$store.dispatch('LOAD_WAPT_JSON')
-  },
-
-  computed: {
-    ...mapGetters([
-      'hostsOkStateCount',
-      'hostsErrorStateCount',
-      'hostsWarnStateCount'
-    ]),
-    data () { return [['ok', this.$store.getters.hostsOkStateCount], ['Warn', this.$store.getters.hostsWarnStateCount], ['Error', this.$store.getters.hostsErrorStateCount]] },
-    count () { return this.$store.state.Dashboard.count },
-    msg () { return this.$store.state.Dashboard.msg }
-  },
-  methods: {
-
-    increment () {
-      this.$store.commit('increment')
-    },
-    PrintThis () {
-      this.$store.commit('ShowTexte')
-    }
+  name: 'Dashboard',
+  components: {
+    pieHostsStatus,
+    serverStatus
   }
 }
 </script>
-
-<style scoped>
-img {
-  width: 30px;
-
-}
-</style>
---
+++ wapt-webui/src/components/dashboard/pieHostsStatus.vue
@@ -0,0 +1,38 @@
+<template>
+  <pie-chart :data="data" :colors="['#4e7300', '#ffa600', '#b51414']" :discrete="true"></pie-chart>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  mounted () {
+    this.$store.dispatch('LOAD_WAPT_JSON')
+  },
+
+  computed: {
+    ...mapGetters([
+      'hostsOkStateCount',
+      'hostsErrorStateCount',
+      'hostsWarnStateCount'
+    ]),
+    data () { return [['ok', this.hostsOkStateCount], ['Warn', this.hostsWarnStateCount], ['Error', this.hostsErrorStateCount]] }
+  },
+  methods: {
+
+    increment () {
+      this.$store.commit('increment')
+    },
+    PrintThis () {
+      this.$store.commit('ShowTexte')
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 30px;
+
+}
+</style>
