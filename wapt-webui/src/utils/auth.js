de9fc34146fa0998f0a532b68fd46ae407b58026
Tim <tim@clerc.im>
2018-06-25 22:51:35 +0200

    Try to get cookie

--- wapt-webui/src/utils/auth.js
+++ wapt-webui/src/utils/auth.js
@@ -1,18 +1,16 @@
 import axios from 'axios'
 
+// axios.defaults.withCredentials = true
+
 export default {
   // Send a request to the login URL and returned reponse in Promise
   login (credentials) {
-    return axios.post(process.env.API_URL + 'v3/login', {
-      params: {
-        user: credentials['username'],
-        password: credentials['password']
-      },
-      headers : {
-        'Content-type' : 'application/form-url-encode'
-      }
-    })
-      .then((response) => Promise.resolve(response.data))
+    return axios.post(process.env.API_URL + 'v3/login?user=' +
+    credentials['username'] + '&password=' + credentials['password'])
+      .then((response) => {
+        console.log(response)
+        Promise.resolve(response.data)
+      })
       .catch((error) => console.log(error))
   }
 }
