825ffa08da5bd92e13748dabfc161080090c99de
Timothée Clerc <tim@clerc.im>
2018-07-03 13:57:31 +0200

    test cookie

--- wapt-webui/src/utils/auth.js
+++ wapt-webui/src/utils/auth.js
@@ -1,16 +1,25 @@
-import axios from 'axios'
+// import axios from 'axios'
+import 'whatwg-fetch'
 
-axios.defaults.withCredentials = true
+// axios.defaults.withCredentials = true
 
 export default {
   // Send a request to the login URL and returned reponse in Promise
   login (credentials) {
-    return axios.post(process.env.API_URL + 'v3/login?user=' +
-    credentials['username'] + '&password=' + credentials['password'])
-      .then((response) => {
-        console.log(response)
-        Promise.resolve(response.data)
+    // return axios.post(process.env.API_URL + 'v3/login?user=' +
+    // credentials['username'] + '&password=' + credentials['password'])
+    //   .then((response) => {
+    //     console.log(response)
+    //     Promise.resolve(response.data)
+    //   })
+    //   .catch((error) => console.log(error))
+    return fetch('http://dev.clerc.im/api/' + 'v3/login?user=' + credentials['username'] + '&password=' + credentials['password'], {
+      credentials: 'include'
+    })
+      .then(function (data) {
+        console.log(data)
+      }).catch(function (error) {
+        console.log(error)
       })
-      .catch((error) => console.log(error))
   }
 }
