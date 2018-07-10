c8a1d668eae39a42017c1cdeb88bfe9d9f11303f
Timothée Clerc <tim@clerc.im>
2018-07-10 18:25:09 +0200

    fetch

--- wapt-webui/src/utils/auth.js
+++ wapt-webui/src/utils/auth.js
@@ -1,25 +1,20 @@
-// import axios from 'axios'
 import 'whatwg-fetch'
-
-// axios.defaults.withCredentials = true
+import http from '@/utils/http'
 
 export default {
   // Send a request to the login URL and returned reponse in Promise
   login (credentials) {
-    // return axios.post(process.env.API_URL + 'v3/login?user=' +
-    // credentials['username'] + '&password=' + credentials['password'])
-    //   .then((response) => {
-    //     console.log(response)
-    //     Promise.resolve(response.data)
-    //   })
-    //   .catch((error) => console.log(error))
-    return fetch('http://dev.clerc.im/api/' + 'v3/login?user=' + credentials['username'] + '&password=' + credentials['password'], {
+    fetch(process.env.API_URL + 'v3/login?user=' + credentials['username'] +
+    '&password=' + credentials['password'], {
       credentials: 'include'
     })
-      .then(function (data) {
-        console.log(data)
-      }).catch(function (error) {
-        console.log(error)
+      .then(http.checkStatus)
+      .then(http.parseJSON)
+      .then((data) => {
+        return data
+      })
+      .catch((error) => {
+        return error
       })
   }
 }
