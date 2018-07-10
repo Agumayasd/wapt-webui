6c4690456ae2da0e3be3f37ffda46ad78d558007
Tim <tim@clerc.im>
2018-07-10 23:16:27 +0200

    Re-indent code

--- wapt-webui/src/components/Login.vue
+++ wapt-webui/src/components/Login.vue
@@ -4,39 +4,47 @@
     <b-container>
       <!--  Username input -->
       <b-form class="login-box">
-        <b-form-group id="usernameInputGroup"
-        label="Username:" label-for="usernameInput">
-        <b-form-input id="usernameInput"
-        type="text" required
-        placeholder="Enter login"
-        autofocus
-        v-model="credentials.username"
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group
+        id="usernameInputGroup"
+        label="Username:"
+        label-for="usernameInput"
+        >
+          <b-form-input
+          id="usernameInput"
+          type="text" required
+          placeholder="Enter login"
+          autofocus
+          v-model="credentials.username"
+          />
+        </b-form-group>
 
-      <!--  Password input -->
-      <b-form-group id="passwordInputGroup"
-      label="Password:" label-for="passwordInput">
-      <b-form-input id="passwordInput"
-      type="password" required
-      placeholder="Enter password"
-      v-model="credentials.password"
-      ></b-form-input>
-    </b-form-group>
+        <!--  Password input -->
+        <b-form-group
+        id="passwordInputGroup"
+        label="Password:"
+        label-for="passwordInput"
+        >
+          <b-form-input
+          id="passwordInput"
+          type="password"
+          required
+          placeholder="Enter password"
+          v-model="credentials.password"
+          />
+        </b-form-group>
 
-    <b-button id="loginButton" class="login-button" type="button" variant="primary" @click="login(credentials)">Login</b-button>
-  </b-form>
-
-  <!--  Display errors under login form -->
-  <!-- <b-alert variant="danger"
-  dismissible
-  :show="showAlert"
-  @dismissed="hideAlert">
-  {{ errors }} -->
-<!-- </b-alert> -->
-
-</b-container>
-</div>
+        <b-button
+        id="loginButton"
+        class="login-button"
+        type="button"
+        variant="primary"
+        @click="login(credentials)"
+        >
+        Login
+        </b-button>
+      </b-form>
+    </b-container>
+  </div>
 </template>
 
 <script>
