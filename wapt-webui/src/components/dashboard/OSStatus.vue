d85bb0a4d5d58d6818b50b84a364dcf5a688ef15
Aguay-val <baraise.valentin@gmail.com>
2018-07-11 13:23:02 +0200

    Refactoring OSStatus component. Using loop to iterate over Windows Version.

--- wapt-webui/src/components/dashboard/OSStatus.vue
+++ wapt-webui/src/components/dashboard/OSStatus.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="colorbg">
     <h4 class="center">Os Inventory List</h4>
-    <hr/>
-    <pie-chart style="padding:7px;" :data="[['win7', Win7Count()], ['win8', Win8Count()], ['win10', Win10Count()]]" :colors="['#01C3CC', '#0C83D1', '#135ED4']"></pie-chart>
+  <hr/>
+    <pie-chart style="padding:7px;" :data=dataOS() :colors="['#01C3CC', '#0C83D1', '#135ED4']"></pie-chart>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    Win7Count () {
-      return this.$store.state.Wapt.waptState.filter(computer => computer.os_version.startsWith('6.1')).length
-    },
-    Win8Count () {
-      return this.$store.state.Wapt.waptState.filter(computer => computer.os_version.startsWith('6.3')).length
-    },
-    Win10Count () {
-      return this.$store.state.Wapt.waptState.filter(computer => computer.os_version.startsWith('10.0')).length
+    dataOS () {
+      var windowsVersion = { 'Win7': '6.1',
+        'Win8': '6.3',
+        'Win10': '10.0'}
+      var arrayOSRepartition = []
+      for (var name in windowsVersion) {
+        var localArray = []
+        var OsNumber = windowsVersion[name]
+        var osCount = this.$store.state.Wapt.waptState.filter(computer => computer.os_version.startsWith(OsNumber)).length
+        // console.log("datum :  " + datum);
+        localArray.push(name)
+        localArray.push(osCount)
+        arrayOSRepartition.push(localArray)
+      }
+      return arrayOSRepartition
     }
   }
 }
@@ -27,8 +34,6 @@ export default {
 .colorbg {
   background-color: lightgray;
   border-radius: 10px;
-  /* width: 545px; */
-  /* height: 331px; */
 }
 
 .center {
